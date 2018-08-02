2d3091861c9b79a39adc603d8870418c833d090c
Your Name <you@example.com>
2018-08-02 17:54:58 -0300

    better clean and comment

--- src/main.f90
+++ src/main.f90
@@ -8,16 +8,17 @@ integer j
 i=1
 ! j is zero for default
 j=0
+
 ! let the tests begin
   if ((i .ne. 2) .and. (i .ne. 3)) then
-    j=1 !triggered
+    j=1 ! if #1a triggered
     if (i .eq. 1) then
-      j=1 !triggered
-    else !i .ne. 1 .or. isnan(i)
-      j=2 !if #? not triggered
+      j=1 ! if #1a1a triggered
+    else !i .ne. 1
+      j=2 ! if #1a1b not triggered
     endif
   else !i .eq. 2 .or. i .eq. 3 .or. isnan(i)
-      j=3 !if #? not triggered
+      j=3 !if #1b not triggered
   endif
 
 
@@ -25,7 +26,7 @@ j=0
 select case(i)
 case(1)
 j=1 !case #1 triggered
-case default
+case default !i .ne. 1
 j=4 !case #2 not triggered
 end select
 
@@ -35,7 +36,7 @@ contains
 
 subroutine sub1()
 !lets also see an issue with allocatables
-integer, allocatable, dimension(:) :: k !why does the gdb stop here?
+integer, allocatable, dimension(:) :: k !why does gdb step in this line?
 allocate(k(9))
 end subroutine
 
